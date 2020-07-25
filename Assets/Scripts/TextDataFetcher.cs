7b2b59d4be2b54e1590d1c49d066c18252558fce
kumusan <ryogi0200@gmail.com>
2020-07-26 01:42:50 +0900

    💥init

---
+++ Assets/Scripts/TextDataFetcher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDataFetcher : MonoBehaviour
+{
+    public Text resultMessaegeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultMessaegeText.text = DataSender.resultMessage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
